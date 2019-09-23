49d929c102d1490918716025c80249b166337abc
Scott <Scotthall1983@hotmail.com>
2019-09-23 21:56:54 +0100

    Added a web_helper file to features directory, added the visit home page, fill in player one and two and click submit button to a sign_in_and_play method. Replaced that repeated code with the new method to enter_name and hit points spec files

--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
 feature 'Enter names' do
   scenario 'submitting names' do
-  visit('/')
-  fill_in :player_1_name, with: "Scott"
-  fill_in :player_2_name, with: "Frankie"
-  click_button 'Submit'
+  sign_in_and_play
   expect(page).to have_content "Scott vs. Frankie"
   end
 end
--- spec/features/hit_points_spec.rb
+++ spec/features/hit_points_spec.rb
@@ -1,9 +1,6 @@
 feature 'seeing player 2s score' do
   scenario 'the page should render player 2 score' do
-    visit('/')
-    fill_in :player_1_name, with: "Scott"
-    fill_in :player_2_name, with: "Frankie"
-    click_button 'Submit'
+    sign_in_and_play
     expect(page).to have_content "Frankie: 100 points"
   end
 end
